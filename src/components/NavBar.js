9f9cbce7f6cbff015c4e13936cb7a8c52e36600f
minm11 <sid30447@gmail.com>
2023-10-07 12:40:11 +0800

    nabar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./NavBar.css";
+//import {AiOutlineMenu} from 'react-icons/ai'
+//import {AiOutlineSearch} from 'react-icons/ai'
+
+
+export default function NavBar(){
+    //3 icons menu search notification
+    <div id="Nav" className="Navbar">
+        <div className="block">
+            <h1>Categories</h1>
+        </div>
+        <div className="block searchBarContainer">
+            <input
+                className="searchbar"
+                type="text"
+            />
+        </div>
+    </div>
+}
